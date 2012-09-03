2e6c84e7b9150bf4ecc5bcc9d324e61fb9f8cceb
reus <reusee@gmail.com>
2012-09-03 23:45:21 +0800

    update

--- emacs.el
+++ emacs.el
@@ -1,13 +1,13 @@
+; Package
 (require 'package)
-(require 'cl)
 (package-initialize)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 ; System
+(require 'cl)
 (when window-system
   (setq x-select-enable-clipboard t)
   (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
@@ -15,62 +15,62 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
-(add-hook 'server-switch-hook (lambda ()
+(add-hook 'server-switch-hook (lambda () ; client模式下，关闭buffer时不提示
   (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)))
-(add-to-list 'auto-mode-alist (cons (file-truename "~/.emacs") 'emacs-lisp-mode))
+(add-to-list 'auto-mode-alist (cons (file-truename "~/.emacs") 'emacs-lisp-mode)) ; .emacs是symlink的话也自动进入emacs-lisp-mode
 (setq compile-command "")
 (helm-mode 1)
 
 ; shell
 (setenv "SHELL" "bash")
-(defadvice shell (around always-new-shell activate) ; always open new buffer
+(defadvice shell (around always-new-shell activate) ; 重命名新打开的buffer
   (let ((buffer (generate-new-buffer-name "*shell*"))) ad-do-it))
-(defadvice term-sentinel (around my-advice-term-sentinel (proc msg) activate) ; auto close buffer when exit shell
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg) activate) ; ansi-term里exit命令会关闭buffer
   (if (memq (process-status proc) '(signal exit))
       (let ((buffer (process-buffer proc)))
         ad-do-it
         (kill-buffer buffer))
     ad-do-it))
-(defvar my-term-shell "/bin/bash") ; do not ask for which shell to run
+(defvar my-term-shell "/bin/bash") ; 执行ansi-term时不询问采用的shell
 (defadvice ansi-term (before force-bash activate)
   (interactive (list my-term-shell)))
-(defun my-term-hook () ; show clickable url
+(defun my-term-hook () ; 让ansi-term里的url可以点击
   (goto-address-mode))
 (add-hook 'term-mode-hook 'my-term-hook)
 
-; minor mode to override keys globally
+; Override Minor Mode
 (defvar my-keys-mode-map (make-keymap) "my-keys-mode keymap.")
 (define-minor-mode my-keys-mode
   t " my-keys" 'my-keys-mode-map)
 (my-keys-mode 1)
-(defun my-minibuffer-setup-hook ()
+(defun my-minibuffer-setup-hook () ; minibuffer里不采用此minor mode
   (my-keys-mode 0))
 (add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
 
 ; Appearance
 (load-theme 'Amelie t)
-(setq ring-bell-function 'ignore)
-(setq scroll-margin 0 scroll-step 1 scroll-conservatively 10000)
-(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore) ; 禁用蜂鸣器
+(setq scroll-margin 0 scroll-step 1 scroll-conservatively 10000) ; 平滑滚动
+(setq inhibit-startup-message t) ; 不显示启动页面
 (when window-system
-  (setq mouse-wheel-scroll-amount '(3 ((shift) . 1)))
-  (setq mouse-wheel-progressive-speed nil)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
+  (setq mouse-wheel-scroll-amount '(3 ((shift) . 1))) ; 鼠标滚动行数
+  (setq mouse-wheel-progressive-speed nil) ; 鼠标没有加速
+  (tool-bar-mode -1) ; 禁用工具栏
+  (scroll-bar-mode -1) ; 禁用滚动条
 )
-(menu-bar-mode -1)
-(blink-cursor-mode -1)
-(set-face-attribute 'default nil :font "Monaco-14")
-(show-paren-mode 1)
+(menu-bar-mode -1) ; 禁用菜单栏
+(blink-cursor-mode -1) ; 禁止光标闪烁
+(set-face-attribute 'default nil :font "Monaco-14") ; 默认字体
+(show-paren-mode 1) ; 高亮显示匹配的括号
 (setq show-paren-delay 0)
 (set-face-foreground 'show-paren-match-face "red")
-(global-hl-line-mode 1)
+(global-hl-line-mode 1) ; 高亮当前行
 (set-face-attribute hl-line-face nil :background "#111")
-(setq evil-flash-delay 36000)
-(set-cursor-color "white")
-(ansi-color-for-comint-mode-on)
-(setq redisplay-dont-pause t)
-(load "~/.emacs.d/my-modeline.el")
+(setq evil-flash-delay 36000) ; 搜索结果高亮时间
+(set-cursor-color "white") ; 光标颜色
+(ansi-color-for-comint-mode-on) ; shell和term下显示ansi标准的颜色
+(setq redisplay-dont-pause t) ; 据说可以加速渲染
+(load "~/.emacs.d/my-modeline.el") ; modeline的定义
 
 ; auto complete
 (require 'auto-complete-config)
@@ -78,7 +78,7 @@
 (setq-default ac-sources '(ac-source-words-in-same-mode-buffers
                            ac-source-abbrev
                            ac-source-dictionary))
-(setq ac-ignore-case nil)
+(setq ac-ignore-case nil) ; 自动完成时，大小写敏感
 
 ; Navigation
 (require 'ace-jump-mode)
@@ -90,33 +90,33 @@
 (set-keyboard-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
-(setq vc-follow-symlinks t)
-(toggle-diredp-find-file-reuse-dir 1)
+(setq vc-follow-symlinks t) ; 打开被版本控制系统控制的文件时，不询问
+(toggle-diredp-find-file-reuse-dir 1) ; 用dired打开文件时，使用当前buffer，而不是新建一个
 (require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward uniquify-separator ":")
+(setq uniquify-buffer-name-style 'post-forward uniquify-separator ":") ; 打开同名文件时，加上目录名作为buffer名
 (setq bookmark-default-file "~/.emacs.d/bookmarks" bookmark-save-flag 1)
-(setq delete-by-moving-to-trash t)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq delete-by-moving-to-trash t) ; 使用系统垃圾箱删除文件
+(add-hook 'before-save-hook 'delete-trailing-whitespace) ; 保存之前删除行尾的空格
 
 ; Backup
 (setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
-(setq version-control t)
-(setq kept-old-versions 2)
-(setq kept-new-versions 256)
-(setq delete-old-versions t)
+(setq version-control t) ; 开启备份文件版本控制
+(setq kept-old-versions 2) ; 保存最初的文件
+(setq kept-new-versions 256) ; 保存最新的文件
+(setq delete-old-versions t) ; 删除不符合以上条件的文件
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
 ; Buffer
 (iswitchb-mode t)
 (require 'tabbar)
-(setq tabbar-buffer-groups-function ; group buffers in tabbar
+(setq tabbar-buffer-groups-function ; tabbar中的buffer分组
       (lambda ()
         (list (cond
                ((string-equal "*" (substring (buffer-name) 0 1)) "System")
                (t "User")
                ))))
-(setq tabbar-buffer-list-function ; filter buffer list in tabbar
+(setq tabbar-buffer-list-function ; tabbar中的buffer过滤
       (lambda ()
         (delq nil
               (mapcar #'(lambda (b)
@@ -128,6 +128,7 @@
                                                     "*Help*"
                                                     "*Packages*"
                                                     "*Completions*"
+                                                    "*Compile-Log*"
                                                     )) nil)
                            ((eq (current-buffer) b) b)
                            ((buffer-file-name b) b)
@@ -141,7 +142,7 @@
 (set-face-attribute 'tabbar-highlight nil :background "black" :foreground "white" :box nil)
 (set-face-attribute 'tabbar-button nil :background "black" :foreground "gray70" :box nil)
 (set-face-attribute 'tabbar-separator nil :background "black" :foreground "white" :box nil)
-(defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate)
+(defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate) ; 修改过的文件，tabbar上显示+号
   (setq ad-return-value
         (if (and (buffer-modified-p (tabbar-tab-value tab))
                  (buffer-file-name (tabbar-tab-value tab)))
@@ -156,44 +157,34 @@
 (add-hook 'after-save-hook 'ztl-modification-state-change)
 (add-hook 'after-revert-hook 'ztl-modification-state-change)
 (add-hook 'first-change-hook 'ztl-on-buffer-modification)
-(mouse-wheel-mode 1)
+(mouse-wheel-mode 1) ; 启用鼠标滚轮
 (tabbar-mode 1)
-(tabbar-mwheel-mode -1)
-(define-key my-keys-mode-map "\C-l" 'tabbar-forward-tab)
+(tabbar-mwheel-mode -1) ; 禁用鼠标滚轮切换tab
+(define-key my-keys-mode-map "\C-l" 'tabbar-forward-tab) ; 全局的tab控制键
 (define-key my-keys-mode-map "\C-k" 'tabbar-backward-tab)
 (define-key my-keys-mode-map "\C-j" 'tabbar-forward-group)
-(add-hook 'minibuffer-exit-hook ; auto close some completion buffer
-          '(lambda ()
-             (mapcar (lambda (buffer)
-                       (when (get-buffer buffer)
-                         (kill-buffer buffer)))
-                     '(
-                       "*Completions*"
-                       "*Ido Completions*"
-                       ))))
 
 ; Editing
-(setq-default indent-tabs-mode nil)
-(setq evil-shift-width 2)
+(setq-default indent-tabs-mode nil) ; 禁用tab缩进
+(setq evil-shift-width 2) ; 缩进量
 (setq-default tab-width 2)
 (setq-default tab-stop-list (number-sequence 2 300 2))
-(add-hook 'python-mode-hook '(lambda ()
+(add-hook 'python-mode-hook '(lambda () ; 设python模式下的缩进量
                                (setq python-indent 2)))
-(electric-pair-mode 1)
+(electric-pair-mode 1) ; 自动输入成对的括号
 
 ; Load evil
-(add-to-list 'load-path "~/.emacs.d/evil")
-(setq evil-default-cursor t)
+(setq evil-default-cursor t) ; 使用主题默认的光标
 (require 'evil)
 (evil-mode 1)
 
 ; mode line color
-(add-hook 'evil-normal-state-entry-hook 'set-mode-line-color)
+(add-hook 'evil-normal-state-entry-hook 'set-mode-line-color) ; 进入另一个模式时，自动切换modeline的颜色
 (add-hook 'evil-insert-state-entry-hook 'set-mode-line-color)
 (add-hook 'evil-replace-state-entry-hook 'set-mode-line-color)
 (add-hook 'evil-visual-state-entry-hook 'set-mode-line-color)
 (add-hook 'evil-operator-state-entry-hook 'set-mode-line-color)
-(defadvice switch-to-buffer (after set-mode-line-color-when-switch activate)
+(defadvice switch-to-buffer (after set-mode-line-color-when-switch activate) ; 切换buffer时自动切换modeline的颜色
   (set-mode-line-color))
 (defun set-mode-line-color ()
   (interactive)
@@ -213,10 +204,6 @@
    ((evil-emacs-state-p)
     (set-face-background 'modeline "#c90"))
     ))
-
-; Remote
-(setq tramp-default-method "ssh")
-(setq tramp-verbose 10)
 
 ; KEYS
 
@@ -256,9 +243,9 @@
 
 ; mode
 (define-key evil-visual-state-map "q" 'evil-force-normal-state)
-(define-key evil-insert-state-map "k" 'cofi/kd)
+(define-key evil-insert-state-map "k" 'cofi/kd) ; 用kd返回normal状态
 (define-key evil-replace-state-map "k" 'cofi/kd)
-(define-key evil-insert-state-map "j" 'cofi/jj)
+(define-key evil-insert-state-map "j" 'cofi/jj) ; 用jj返回normal状态
 (define-key evil-replace-state-map "j" 'cofi/jj)
 (evil-define-command cofi/kd () (interactive) (cofi/maybe-normal "k" ?d))
 (evil-define-command cofi/jj () (interactive) (cofi/maybe-normal "j" ?j))
